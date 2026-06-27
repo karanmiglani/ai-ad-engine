0fc5125429b0451dd314d95ef9df1c2907b88350
karanmiglani <52616845+karanmiglani@users.noreply.github.com>
2026-06-27 11:39:13 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/karanmiglani/contextiq/ContextiqApplication.java
@@ -0,0 +1,13 @@
+package com.karanmiglani.contextiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContextiqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContextiqApplication.class, args);
+	}
+
+}
